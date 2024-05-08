4dc0076c6b32c4f22ca5f34311fe1da5af927e88
Kovalski <andre.kovalski@tecpuc.com.br>
2024-05-08 19:06:05 -0300

    adicionando task para controlar a direção, utilizando semaforo binario para acesso à direção dos motores

--- main_.c
+++ main_.c
@@ -3,11 +3,18 @@
 #include "basic_io.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
+#include "semphr.h"
 
+const uint8_t MAX_SPEED = 10;
 
-int32_t driver_0, driver_1, driver_2, driver_3 = 0;
-uint8_t delayMult = 1;
+volatile SemaphoreHandle_t s_driver;
+volatile SemaphoreHandle_t s_controls;
+uint8_t orientacao_manobras = 0b011; 
+volatile int8_t driver_0, driver_1, driver_2, driver_3 = 0;
+uint8_t delayMult = 10;
+
 /*
  1 - O protótipo da função de uma tarefa deve sempre retornar void, ou seja, a tarefa não possui retorno.
  2 - A função de uma tarefa deve receber um parâmetro de ponteiro void. 
@@ -15,7 +22,17 @@ uint8_t delayMult = 1;
 void vTask1_guinada(void *pvParameters);
 void vTask2_arfagem(void *pvParameters);
 void vTask3_rolagem(void *pvParameters);
-void vTask4_prints(void *pvParameters);
+void vTask4_control(void *pvParameters);
+void vTask5_printer(void *pvParameters);
+
+void acc_driver(volatile int8_t *driver);
+void dec_driver(volatile int8_t *driver);
+
+void printb(uint8_t v);
+void printb(uint8_t v) {
+    uint8_t i, s = 1<<((sizeof(v)<<3)-1);
+    for (i = s; i; i>>=1) printf("%d", v & i || 0 );
+}
 
 /*
  Denição da estrutura da função
@@ -27,26 +44,26 @@ void vTask1_guinada(void *pvParameters)
     // Mascara utilizada para checagem do estado da ação (no caso, Guinada)
     const uint8_t isHorario = 0b001;
     
-    // Recebe o parametro da função como um inteiro de 64 bits (exageiro demais)
-    uint64_t param = (uint64_t) pvParameters;
+    // Recebe o parametro da função como um inteiro de 8 bits 
+    volatile uint8_t* param =  pvParameters;
 
     // for infinito
     for(;;)
     {
-        vPrintString("Guinada\n");
-
         // verifica se isHorario está de acordo com a máscara, e sim, é Guinada em sentido horario, caso contrario, anti-horario
-        if(isHorario&param){
-            driver_0++;
-            driver_1--;
-            driver_2++;
-            driver_3--;
+        xSemaphoreTake(s_driver, portMAX_DELAY);
+        if(isHorario&(*param)){
+            acc_driver(&driver_0);
+            dec_driver(&driver_1);
+            acc_driver(&driver_2);
+            dec_driver(&driver_3);
         } else {
-            driver_0--;
-            driver_1++;
-            driver_2--;
-            driver_3++;
+            dec_driver(&driver_0);
+            acc_driver(&driver_1);
+            dec_driver(&driver_2);
+            acc_driver(&driver_3);
         }
+        xSemaphoreGive(s_driver);
 
         // delay de 10ms, o delayMult é para auxiliar durante o debug das tasks
         vTaskDelay(10*delayMult);
@@ -60,22 +77,23 @@ void vTask1_guinada(void *pvParameters)
 void vTask2_arfagem(void* pvParameters)
 {
     const uint8_t isFront = 0b010;
-    uint64_t param = (uint64_t) pvParameters;
+    volatile uint8_t* param = pvParameters;
 
     for(;;)
     {
-        vPrintString("Arfagem\n");
-        if(isFront&param){
-            driver_0--;
-            driver_1--;
-            driver_2++;
-            driver_3++;
+        xSemaphoreTake(s_driver, portMAX_DELAY);
+        if(isFront&(*param)){
+            dec_driver(&driver_0);
+            dec_driver(&driver_1);
+            acc_driver(&driver_2);
+            acc_driver(&driver_3);
         } else {
-            driver_0++;
-            driver_1++;
-            driver_2--;
-            driver_3--;
+            acc_driver(&driver_0);
+            acc_driver(&driver_1);
+            dec_driver(&driver_2);
+            dec_driver(&driver_3);
         }
+        xSemaphoreGive(s_driver);
         vTaskDelay(40*delayMult);
     }
 	vTaskDelete(NULL);
@@ -85,52 +103,75 @@ void vTask2_arfagem(void* pvParameters)
 void vTask3_rolagem(void* pvParameters)
 {
     const uint8_t isLeft = 0b100;
-    uint64_t param = (uint64_t) pvParameters;
+    volatile uint8_t* param = pvParameters;
 
     for(;;)
     {
-        vPrintString("Rolagem\n");
-        if(isLeft&param){
-            driver_0++;
-            driver_1--;
-            driver_2--;
-            driver_3++;
+        xSemaphoreTake(s_driver, portMAX_DELAY);
+        if(isLeft&(*param)){
+            acc_driver(&driver_0);
+            dec_driver(&driver_1);
+            dec_driver(&driver_2);
+            acc_driver(&driver_3);
         } else {
-            driver_0--;
-            driver_1++;
-            driver_2++;
-            driver_3--;
+            dec_driver(&driver_0);
+            acc_driver(&driver_1);
+            acc_driver(&driver_2);
+            dec_driver(&driver_3);
         }
+        xSemaphoreGive(s_driver);
         vTaskDelay(20*delayMult);
     }
 	vTaskDelete(NULL);
 }
 
-// task para printar o estado do param e dos motores
-void vTask4_prints(void *pvParameters){
-    for(;;){
-        printf("param:[%"PRIu64"]\n", (uint64_t)pvParameters);
-        printf("drive_0:[%"PRId32"]\n", driver_0);
-        printf("drive_1:[%"PRId32"]\n", driver_1);
-        printf("drive_2:[%"PRId32"]\n", driver_2);
-        printf("drive_3:[%"PRId32"]\n\n", driver_3);
-
-        // faz parse do parametro para um unsigned int de 64 bits, inverte os bits dele e da parte para um pointeiro para void, e modifica o valor do ponteiro usado para passar o parametro
-        vTaskDelay(10*delayMult);
+void vTask4_control(void *pvParameters){
+    volatile uint8_t* param = pvParameters;
+    for(;;)
+    {
+        xSemaphoreTake(s_driver, portMAX_DELAY);
+        *param ^= (rand() % 8);
+        xSemaphoreGive(s_driver);
+        vTaskDelay(90*delayMult);
     }
-    vTaskDelete(NULL);
+	vTaskDelete(NULL);
+}
+
+void vTask5_printer(void *pvParameters){
+    for(;;)
+    {
+        xSemaphoreTake(s_driver, portMAX_DELAY);
+        printf("param[%d]\n", orientacao_manobras%8);
+        printf("Motores:\n\t0:[%d]\t1:[%d]\n\n\t2:[%d]\t3:[%d]\n", driver_0, driver_1, driver_2, driver_3);
+        xSemaphoreGive(s_driver);
+        vTaskDelay(100*delayMult);
+    }
+	vTaskDelete(NULL);
+}
+
+void acc_driver(volatile int8_t *driver){
+    if(*driver < MAX_SPEED){
+        *driver+=1;
+    }
+    return;
+}
+void dec_driver(volatile int8_t *driver){
+    if(*driver > -MAX_SPEED){
+        *driver-=1;
+    }
+    return;
 }
 
 int main_(void)
 {
-    // gostaria que as orientações estivessem em um uint8_t, mas não tem muito o que fazer com ponteiro de void
-    uint64_t orientacao_manobras = 0b011;
-	xTaskCreate(vTask1_guinada, "Guinada", 1000, (void*)orientacao_manobras, 1, NULL);
-	xTaskCreate(vTask2_arfagem, "Arfagem", 1000, (void*)orientacao_manobras, 1, NULL);
-	xTaskCreate(vTask3_rolagem, "Rolagem", 1000, (void*)orientacao_manobras, 1, NULL);
-	xTaskCreate(vTask4_prints, "Prints", 1000, (void*)orientacao_manobras, 1, NULL);
-    
+    vSemaphoreCreateBinary(s_driver);
 
+	xTaskCreate(vTask1_guinada, "Guinada", 1000, &orientacao_manobras, 1, NULL);
+	xTaskCreate(vTask2_arfagem, "Arfagem", 1000, &orientacao_manobras, 1, NULL);
+	xTaskCreate(vTask3_rolagem, "Rolagem", 1000, &orientacao_manobras, 1, NULL);
+	xTaskCreate(vTask4_control, "Control", 1000, &orientacao_manobras, 0, NULL);
+	xTaskCreate(vTask5_printer, "Printer", 1000, NULL, 0, NULL);
+    
 	// Inicia o escalonador de tarefas
 	vTaskStartScheduler();
 
